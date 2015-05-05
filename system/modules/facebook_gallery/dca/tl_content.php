fdd6702689b4660de4be886a31b863ef168ce476
fritzmg <github@spikx.net>
2015-05-05 17:05:22 +0200

    Removed per page load callback in DCA

--- system/modules/facebook_gallery/dca/tl_content.php
+++ system/modules/facebook_gallery/dca/tl_content.php
@@ -14,9 +14,6 @@
 
 
 $GLOBALS['TL_DCA']['tl_content']['palettes']['facebookGallery'] = '{type_legend},type,headline;{facebook_legend},fbAlbumId;{image_legend},size,imagemargin,perRow,fullsize,perPage,numberOfItems;{template_legend:hide},galleryTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space;{invisible_legend:hide},invisible,start,stop';
-
-$GLOBALS['TL_DCA']['tl_content']['fields']['perPage']['load_callback'][] = array('tl_content_facebook_gallery','ppLoadCallback');
-
 $GLOBALS['TL_DCA']['tl_content']['fields']['fbAlbumId'] = array
 (
 	'label'     => &$GLOBALS['TL_LANG']['tl_content']['fbAlbumId'],
@@ -25,16 +22,3 @@ $GLOBALS['TL_DCA']['tl_content']['fields']['fbAlbumId'] = array
 	'eval'      => array('tl_class' => 'long','maxlength'=>255),
 	'sql'       => "varchar(255) NOT NULL default ''"
 );
-
-
-class tl_content_facebook_gallery extends Backend
-{
-	public function ppLoadCallback( $varValue, DataContainer $dc )
-	{
-		// we can't fetch more than 100 images for one page
-		if( ( $varValue <= 0 || $varValue > 100 ) && $dc->activeRecord->type == 'facebookGallery' )
-			return 100;
-
-		return $varValue;
-	}
-}
